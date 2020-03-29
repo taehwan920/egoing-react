505443500ca99509bcfd12ed972b5624312d2236
Yang Taehwan <taehwan920@gmail.com>
2020-03-29 23:44:19 +0900

    added tags for CreateContent Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
 import Control from './components/Control';
 import Subject from './components/Subject';
 import TOC from './components/TOC';
+import CreateContent from './components/CreateContent';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: 'read',
+      mode: 'create',
       selected_content_id: 2,
       subject: { title: 'WEB', sub: 'World Wide Web!' },
       welcome: { title: 'Welcome', desc: 'Hello, React!!' },
@@ -23,10 +24,11 @@ class App extends Component {
   }
   render() {
     console.log('App render')
-    let _title, _desc = null;
+    let _title, _desc, _article = null;
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode === 'read') {
       let i = 0;
       while (i < this.state.contents.length) {
@@ -38,6 +40,9 @@ class App extends Component {
         }
         i++
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if (this.state.mode === 'create') {
+      _article = <CreateContent></CreateContent>
     }
     return (
       <div className="App">
@@ -63,7 +68,7 @@ class App extends Component {
             mode: _mode
           });
         }.bind(this)}></Control>
-        <Content title={_title} desc={_desc}></Content>
+        {_article}
       </div >
     );
   };
