769d993b9d3b5c0b4b69bde3e606b372afde974d
Yang Taehwan <taehwan920@gmail.com>
2020-03-29 18:10:59 +0900

    we will add CRUD functions for this web page. now is the first step, Control Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Content from './components/Content';
+import Control from './components/Control';
 import Subject from './components/Subject';
 import TOC from './components/TOC';
 
@@ -57,6 +58,11 @@ class App extends Component {
           }.bind(this)}
           data={this.state.contents}
         ></TOC>
+        <Control onChangeMode={function (_mode) {
+          this.setState({
+            mode: _mode
+          });
+        }.bind(this)}></Control>
         <Content title={_title} desc={_desc}></Content>
       </div >
     );
